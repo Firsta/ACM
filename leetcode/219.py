8ca35c0759dfd8a820b7c5a60048e01fa84db53d
Firsta <firstawhois@gmail.com>
2015-07-28 11:37:35 +0800

    leetcode219

---
+++ leetcode/219.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {boolean}
+    def containsNearbyDuplicate(self, nums, k):
+        dic = {}
+        for index, val in enumerate(nums):
+            if val in dic:
+                if index - dic[val] <= k:
+                    return True
+            dic[val] = index
+        return False
