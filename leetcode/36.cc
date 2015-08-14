4e5099976f95945c0d9d6a6538817d2997da1d09
Firsta <firstawhois@gmail.com>
2015-08-14 10:10:58 +0800

    leetcode 36

---
+++ leetcode/36.cc
@@ -0,0 +1,27 @@
+ass Solution {
+    public:
+        bool isValidSudoku(vector<vector<char>>& board) {
+            int line[9][10], row[9][10], square[9][10];
+            memset(line, 0, sizeof(line));
+            memset(row, 0, sizeof(row));
+            memset(square, 0, sizeof(square));
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++) {
+                    if (board[i][j] != '.') {
+                        int now = board[i][j] - '0';
+                        if (line[i][now] != 0)
+                            return false;
+                        if (row[j][now] != 0)
+                            return false;
+                        if (square[j / 3 + i / 3 * 3][now] != 0)
+                            return false;
+                        line[i][now] = row[j][now] = square[j / 3 + i / 3 * 3][now] = 1;
+
+                    }
+
+                }
+            return true;
+
+        }
+
+};
