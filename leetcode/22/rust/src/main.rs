adc7b95da900109babcb45046a9e84b69f8e361b
yipu3 <5365604+yipu3@users.noreply.github.com>
2024-04-01 21:42:27 +0800

    leetcode 22 in rust

---
+++ leetcode/22/rust/src/main.rs
@@ -0,0 +1,61 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn count_nodes(root: &Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    match root {
+        Some(root) => {
+            let root = root.borrow();
+            count_nodes(&root.left) + count_nodes(&root.right) + 1
+        }
+        None => 0,
+    }
+}
+
+fn main() {
+    assert_eq!(
+        count_nodes(&Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 6,
+                    left: None,
+                    right: None,
+                }))),
+                right: None,
+            }))),
+        })))),
+        6
+    );
+}
