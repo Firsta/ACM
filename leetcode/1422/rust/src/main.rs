4fc036cab6a7af65a3fe66e4b4c8b4266fe6f9c5
yipu3 <5365604+yipu3@users.noreply.github.com>
2025-01-01 11:55:28 +0800

    refine 1422

--- leetcode/1422/rust/src/main.rs
+++ leetcode/1422/rust/src/main.rs
@@ -1,8 +1,8 @@
 use std::cmp::max;
 
-pub fn max_score(s: String) -> i32 {
+pub fn max_score_0(s: String) -> i32 {
     let mut one = s.chars().filter(|x| *x == '1').count();
-    
+
     let mut zero = 0;
     let mut result = 0;
     for (i, c) in s.chars().enumerate() {
@@ -18,6 +18,23 @@ pub fn max_score(s: String) -> i32 {
     result as i32
 }
 
+pub fn max_score(s: String) -> i32 {
+    let one = s.chars().filter(|x| *x == '1').count();
+
+    let mut zero = 0;
+    let mut result = 0;
+    for (i, c) in s.chars().enumerate() {
+        if c == '0' {
+            zero += 1;
+        }
+        if i < s.len() - 1 {
+            result = max(result, zero + one - (i + 1 - zero));
+        }
+    }
+    result as i32
+}
+
 fn main() {
-    println!("{}", max_score("011101".to_string()));
+    assert_eq!(max_score("011101".to_string()), 5);
+    assert_eq!(max_score("1111".to_string()), 3);
 }
